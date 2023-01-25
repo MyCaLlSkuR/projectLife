6dd4ca3638a81b073bcdb8dd896ab27847636ce3
mikhailskuratovich <skuratovich.cniitu@gmail.com>
2023-01-25 09:31:05 +0300

    initial

---
+++ src/main/java/by/skuratovich/projectLife/ProjectLifeApplication.java
@@ -0,0 +1,13 @@
+package by.skuratovich.projectLife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/skuratovich/projectLife/ProjectLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package by.skuratovich.projectLife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
